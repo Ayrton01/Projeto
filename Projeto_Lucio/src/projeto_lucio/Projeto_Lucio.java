4082dce7e91f3d4a23dc6d66920f335502f9f5a2
Ayrton <oayrtonsousa@gmail.com>
2023-09-09 21:27:23 -0400

    novas modificações

--- Projeto_Lucio/src/projeto_lucio/Projeto_Lucio.java
+++ Projeto_Lucio/src/projeto_lucio/Projeto_Lucio.java
@@ -2,19 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication8;
+package projeto_lucio;
 
 /**
  *
  * @author Ayrton
  */
-public class JavaApplication8 {
+public class Projeto_Lucio {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+         Tela_Login tela = new Tela_Login();
+         tela.setVisible(true);
+         
+         tela.setExtendedState(6);
+         
+         tela.pack();
+         tela.setLocationRelativeTo(null);
     }
     
 }
